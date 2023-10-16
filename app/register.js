188a4b334f0b3e18453ec37dc1f8207615f4a000
Prawee Wongsa <prawee@hotmail.com>
2023-10-17 00:31:07 +0700

    create new page with register

---
+++ app/register.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const register = () => {
+    return (
+        <View>
+        <Text>Register Users</Text>
+        </View>
+    )
+}
+
+export default register
